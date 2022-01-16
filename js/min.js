8d8735fb2b83780473ba9ef566212d5f5c299ff7
suzan <asadsuzan7@gmail.com>
2022-01-16 13:44:25 +0600

    add banner section and modifed responsive

--- js/min.js
+++ js/min.js
@@ -1,8 +1,8 @@
-
+// nav bar//
 const toggler = document.getElementById('toggler');
 const navigation = document.getElementById('navigation');
 
 toggler.addEventListener('click',function(){
     navigation.classList.toggle('nav_toggle');
-    console.log(1);
+    
 })
