a883f065082feade2b412b7fa1608ef9688909b9
suzan <asadsuzan7@gmail.com>
2022-01-17 15:03:18 +0600

    added portfolio section

---
+++ js/plugins.js
@@ -0,0 +1,55 @@
+
+      // filtering functions srt
+      $('.list').click(function () {
+        let value = $(this).attr('data-filter');
+        if (value == 'all') {
+            $('.items').show(1000);
+        } else {
+            $('.items').not("." + value).hide(1000);
+            $('.items').filter("." + value).show(1000);
+        };
+
+        $('.list').removeClass('activeee');
+        $(this).addClass('activeee');
+
+    });
+    // filtering functions end
+// magefic poup functions srt
+$('.items-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        // other options
+        gallery: {
+            enabled: true, // set to true to enable gallery
+
+            preload: [0, 2], // read about this option in next Lazy-loading section
+
+            navigateByImgClick: true,
+
+            arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+            tPrev: 'Previous (Left arrow key)', // title for left button
+            tNext: 'Next (Right arrow key)', // title for right button
+            tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+        },
+        mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+        zoom: {
+            enabled: true, // By default it's false, so don't forget to enable it
+
+            duration: 300, // duration of the effect, in milliseconds
+            easing: 'ease-in-out', // CSS transition easing function
+
+            // The "opener" function should return the element from which popup will be zoomed in
+            // and to which popup will be scaled down
+            // By defailt it looks for an image tag:
+            opener: function (openerElement) {
+                // openerElement is the element on which popup was initialized, in this case its <a> tag
+                // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                return openerElement.is('img') ? openerElement : openerElement.find('img');
+            }
+        }
+    });
+    // magefic poup functions end
+
+
